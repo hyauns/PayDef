163fae1ca16ded72dfa954a537155375c3666839
deptrai <hyauns@gmail.com>
2026-05-02 23:28:46 +0700

    chore: cleanup scratch scripts

--- scripts/run.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL_UNPOOLED });
-pool.query(`
-      SELECT * FROM transactions WHERE id = 'e737c3da-7d92-411a-8cff-73ab063db1d7'
-`).then(res => {
-    console.table(res.rows);
-    process.exit(0);
-}).catch(console.error);
